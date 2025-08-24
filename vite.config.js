5b519aedfcc7f62c6c9327e2d17cb7b8b06810fa
Kartik S Nekar <110713801+ksnekar2021@users.noreply.github.com>
2025-08-24 16:58:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/5dportfolio/",
-});
+  plugins: [react()],
+  base: '/5dportfolio/'   // 👈 add this line
+})
